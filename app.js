282fa5cb54d4c8f2c45980800fc8ff17830e07e4
GrooshBene <wltn000129@gmail.com>
2017-06-03 18:11:40 +0900

    coin.js route file added to app.js

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.set('view engine', 'jade');
 
 //Load Modules
 require('./routes/auth.js')(app, User, randomString);
+require('./routes/coin.js')(app, Coin, Text, randomString);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
