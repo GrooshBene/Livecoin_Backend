b29e9b37f58db87900d1673d26ae771245beb91a
GrooshBene <wltn000129@gmail.com>
2017-06-05 22:57:01 +0900

    coin.js module exporting done, like/dislike route done

---
+++ routes/coin.js
@@ -0,0 +1,11 @@
+module.exports = init;
+function init(app, Coin, randomString){
+    app.post('/coin/like', function(req, res){
+        
+    });
+
+    app.post('/coin/dislike', function(req, res){
+
+    })
+
+}
