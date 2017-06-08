54f53afc1d635ebe71064d28f392f965d79912da
GrooshBene <wltn000129@gmail.com>
2017-06-08 11:27:56 +0900

    Daily Info Updating function added

--- routes/coin.js
+++ routes/coin.js
@@ -2,7 +2,7 @@ module.exports = init;
 function init(app, Coin, randomString){
     var poloniex_api = require('poloniex-api-node');
     var poloniex = new poloniex_api('KIR1UZCY-AZ8K2TET-BCGD5C6W-HQ4KU5A4','824a2e3e1b276079ea3f625609f033f0dd92104f7fac0b797ea01874827793e20d50d435dc87e4ef7ce878b3b99f70c33b0a4b355ad91bc64fe48bf8147f7213');
-    poloniex.returnLoanOrders('BTC', null, function(err, ticker){
+    poloniex.returnTicker(function(err, ticker){
         if(err){
             throw err;
         }
